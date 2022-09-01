4640c42c7f4d2212125c0c9dabb86f23a0faf75e
Aliaksei-Hrazhynski <101507074+Aliaksei-Hrazhynski@users.noreply.github.com>
2022-09-01 15:34:01 +0200

    add selection sort algorithm

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ def bubbleSort(array):
     return array
     
     
+# selection sort algorithm
+    def selectionSort(array):
+    for i in range(len(array)-1):
+        min_idx = i
+        for idx in range(i + 1, len(array)-1):
+            if array[idx] < array[min_idx]:
+                min_idx = idx
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
+
