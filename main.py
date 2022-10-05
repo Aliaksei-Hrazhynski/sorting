ae89c2d951b4ff72c06a985343647a8e40fda016
Aliaksei-Hrazhynski <mymilofowork@gmail.com>
2022-10-05 08:06:08 +0300

    good

---
+++ main.py
@@ -0,0 +1,26 @@
+# bubble sort algorithm
+# complexity O(n**2)
+def bubbleSort(array):
+    swapped = False
+    for i in range(len(array) - 1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+        if swapped:
+            swapped = False
+        else:
+            break
+    return array
+
+
+# selection sort algorithm
+# complexity O(n**2)
+def selectionSort(array):
+    for i in range(len(array) - 1):
+        min_idx = i
+        for idx in range(i + 1, len(array) - 1):
+            if array[idx] < array[min_idx]:
+                min_idx = idx
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
